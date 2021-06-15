7ae43819854af07ab3652c8c2c6a76bad9ef8635
Kavya2905 <74311514+Kavya2905@users.noreply.github.com>
2021-06-15 17:03:30 +0530

    Create IR_sensor_Digital.ino

---
+++ IR_sensor_Digital.ino
@@ -0,0 +1,13 @@
+void setup() {
+  pinMode(D1, INPUT);
+  Serial.begin(9600);  // baudrate (9600 std in india)  - data transmission speed
+
+}
+
+void loop() {
+  int s = digitalRead(D1); //digitalread converts input to binary
+  Serial.println(s);
+  delay(1000);
+
+}
+© 2021 GitHub, Inc.
